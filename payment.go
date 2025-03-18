9375ee751ba5f9225169ac9d5b3d329afbbc1c95
sn1k3hunt3r <sn1k3hunt3r@gmail.com>
2025-03-18 22:13:54 +0300

    reworked payment

--- payment.go
+++ payment.go
@@ -1,126 +1,107 @@
 package api_client
 
-// package api_client
+import (
+	"errors"
+	"fmt"
+	"time"
 
-// import (
-// 	"errors"
-// 	"fmt"
-// 	"time"
+	types "github.com/snakehunterr/hacs_dbapi_types"
+	"github.com/snakehunterr/hacs_dbapi_types/validators"
+)
 
-// 	types "github.com/snakehunterr/hacs_dbapi_types"
-// 	"github.com/snakehunterr/hacs_dbapi_types/validators"
-// )
+func (c APIClient) PaymentGetAll() ([]types.Payment, error) {
+	var ps []types.Payment
+	err := c.resourceGet(fmt.Sprintf("%s/payment/all", c.baseAPIURL), &ps)
 
-// func (c APIClient) PaymentGetAll() ([]types.Payment, error) {
-// 	var ps []types.Payment
-// 	err := c.resourceGet(fmt.Sprintf("%s/payment/all", c.baseAPIURL), &ps)
+	return ps, err
+}
 
-// 	return ps, err
-// }
+func (c APIClient) PaymentGetAllByClientID(id int64) ([]types.Payment, error) {
+	var ps []types.Payment
+	err := c.resourceGet(fmt.Sprintf("%s/payment/client/id/%d", c.baseAPIURL, id), &ps)
 
-// func (c APIClient) PaymentGetAllByClientID(id int64) ([]types.Payment, error) {
-// 	var ps []types.Payment
-// 	err := c.resourceGet(fmt.Sprintf("%s/payment/client/id/%d", c.baseAPIURL, id), &ps)
+	return ps, err
+}
 
-// 	return ps, err
-// }
+func (c APIClient) PaymentGetAllByRoomID(id int64) ([]types.Payment, error) {
+	var ps []types.Payment
+	err := c.resourceGet(fmt.Sprintf("%s/payment/room/id/%d", c.baseAPIURL, id), &ps)
 
-// func (c APIClient) PaymentGetAllByRoomID(id int64) ([]types.Payment, error) {
-// 	var ps []types.Payment
-// 	r, err := c.resourceGet(fmt.Sprintf("%s/payment/room/id/%d", c.baseAPIURL, id), &ps)
+	return ps, err
+}
 
-// 	return ps, err
-// }
+func (c APIClient) PaymentGetByID(id int64) (*types.Payment, error) {
+	var p types.Payment
+	err := c.resourceGet(fmt.Sprintf("%s/payment/id/%d", c.baseAPIURL, id), &p)
 
-// func (c APIClient) PaymentGetByID(id int64) (*types.Payment, error) {
-// 	var p types.Payment
-// 	r, err := c.resourceGet(fmt.Sprintf("%s/payment/id/%d", c.baseAPIURL, id), &p)
+	if err != nil {
+		return nil, err
+	} else {
+		return &p, nil
+	}
+}
 
-// 	switch {
-// 	case err != nil:
-// 		return nil, nil, err
-// 	case r != nil:
-// 		return nil, r, nil
-// 	default:
-// 		return &p, nil, nil
-// 	}
-// }
+func (c APIClient) PaymentGetByDate(date time.Time) ([]types.Payment, error) {
+	var ps []types.Payment
+	err := c.resourceGet(
+		fmt.Sprintf("%s/payment/date/%s",
+			c.baseAPIURL,
+			date.Format(validators.DATE_FORMAT),
+		),
+		&ps,
+	)
 
-// func (c APIClient) PaymentGetByDate(date time.Time) ([]types.Payment, error) {
-// 	var ps []types.Payment
-// 	r, err := c.resourceGet(
-// 		fmt.Sprintf("%s/payment/date/%s",
-// 			c.baseAPIURL,
-// 			date.Format(validators.DATE_FORMAT),
-// 		),
-// 		&ps,
-// 	)
+	return ps, err
+}
 
-// 	switch {
-// 	case err != nil:
-// 		return nil, nil, err
-// 	case r != nil:
-// 		return nil, r, nil
-// 	default:
-// 		return ps, nil, nil
-// 	}
-// }
+func (c APIClient) PaymentGetByDateRange(date_start time.Time, date_end time.Time) ([]types.Payment, error) {
+	var ps []types.Payment
+	err := c.resourcePost(
+		fmt.Sprintf("%s/payment/date/range", c.baseAPIURL),
+		Form{
+			"date_start": date_start.Format(validators.DATE_FORMAT),
+			"date_end":   date_end.Format(validators.DATE_FORMAT),
+		},
+		&ps,
+	)
 
-// func (c APIClient) PaymentGetByDateRange(date_start time.Time, date_end time.Time) ([]types.Payment, error) {
-// 	var ps []types.Payment
-// 	r, err := c.resourcePost(
-// 		fmt.Sprintf("%s/payment/date/range", c.baseAPIURL),
-// 		Form{
-// 			"date_start": date_start.Format(validators.DATE_FORMAT),
-// 			"date_end":   date_end.Format(validators.DATE_FORMAT),
-// 		},
-// 		&ps,
-// 	)
+	return ps, err
+}
 
-// 	switch {
-// 	case err != nil:
-// 		return nil, nil, err
-// 	case r != nil:
-// 		return nil, r, nil
-// 	default:
-// 		return ps, nil, nil
-// 	}
-// }
+func (c APIClient) PaymentCreate(client_id int64, room_id int64, date time.Time, amount float64) (*types.Payment, error) {
+	var (
+		p = types.Payment{
+			ClientID: client_id,
+			RoomID:   room_id,
+			Date:     date,
+			Amount:   amount,
+		}
+	)
 
-// func (c APIClient) PaymentCreate(p *types.Payment) error {
-// 	if p == nil {
-// 		return nil, errors.New("*types.Payment is nil")
-// 	}
+	err := c.resourceCreateWithDecode(
+		fmt.Sprintf("%s/payment/new", c.baseAPIURL),
+		formatPayment(&p),
+		&p,
+	)
 
-// 	var _p types.Payment
-// 	r, err := c.resourceCreateDecode(
-// 		fmt.Sprintf("%s/payment/new", c.baseAPIURL),
-// 		formatPayment(p),
-// 		&_p,
-// 	)
+	if err != nil {
+		return nil, err
+	} else {
+		return &p, nil
+	}
+}
 
-// 	switch {
-// 	case err != nil:
-// 		return nil, err
-// 	case r != nil:
-// 		return r, nil
-// 	}
+func (c APIClient) PaymentDelete(id int64) error {
+	return c.resourceDelete(fmt.Sprintf("%s/payment/id/%d", c.baseAPIURL, id))
+}
 
-// 	p.ID = _p.ID
-// 	return nil, nil
-// }
+func (c APIClient) PaymentPatch(p *types.Payment) error {
+	if p == nil {
+		return errors.New("*types.Payment is nil")
+	}
 
-// func (c APIClient) PaymentDelete(id int64) error {
-// 	return c.resourceDelete(fmt.Sprintf("%s/payment/id/%d", c.baseAPIURL, id))
-// }
-
-// func (c APIClient) PaymentPatch(p *types.Payment) error {
-// 	if p == nil {
-// 		return nil, errors.New("*types.Payment is nil")
-// 	}
-
-// 	return c.resourcePatch(
-// 		fmt.Sprintf("%s/payment/id/%d", c.baseAPIURL, p.ID),
-// 		formatPayment(p),
-// 	)
-// }
+	return c.resourcePatch(
+		fmt.Sprintf("%s/payment/id/%d", c.baseAPIURL, p.ID),
+		formatPayment(p),
+	)
+}
